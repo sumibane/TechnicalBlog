9dfcec8e0e10a0bf8b091c70a5ee1a1ac8355db7
sumibane <sum123.ban@gmail.com>
2020-09-19 18:10:32 +0530

    Login Module done

--- src/main/java/technicalblog/controller/UserController.java
+++ src/main/java/technicalblog/controller/UserController.java
@@ -1,18 +1,42 @@
 package technicalblog.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import technicalblog.model.Post;
+import technicalblog.model.User;
+import technicalblog.service.PostService;
+
+import java.util.ArrayList;
 
 @Controller
 public class UserController {
-    //For Logging Redirection
+
+    @Autowired
+    private PostService postService;
+
+    //For Logging Landing page
     @RequestMapping("users/login")
     public String login(){
         return "users/login";
+    }
+    //For Logging After page
+    @RequestMapping(value = "users/login", method = RequestMethod.POST)
+    public String loginUser(User user){
+        return "redirect:/posts";
     }
     //For Registration Redirection
     @RequestMapping("users/registration")
     public String registration(){
         return "users/Registration";
+    }
+    //For Logout
+    @RequestMapping(value = "users/logout", method = RequestMethod.POST)
+    public String logout(Model model){
+        ArrayList<Post> posts = postService.getAllPosts();
+        model.addAttribute("posts", posts);
+        return "index";
     }
 }
---
+++ src/main/java/technicalblog/model/User.java
@@ -0,0 +1,14 @@
+package technicalblog.model;
+
+public class User {
+    private String username;
+    private String Password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
